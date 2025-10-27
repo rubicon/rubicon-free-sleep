993a3bf47a75456a3fb199f7a838d38c9ccba035
David <noahmeier82@proton.me>
2025-10-26 17:18:25 -0700

    Add source maps to sentry for the front-end

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,23 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
+import info from '../server/src/serverInfo.json';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), sentryVitePlugin({
+    org: 'free-sleep',
+    project: 'app',
+    release: {
+      name: info.version,
+    }
+  })],
   server: {
     host: '0.0.0.0', // This makes the server accessible to other devices on the network
     port: 5173, // Optional: specify a port if you want something other than the default
   },
   build: {
-    sourcemap: process.env.NODE_ENV === 'development',
+    sourcemap: true,
     outDir: '../server/public/',
     rollupOptions: {
       output: {
